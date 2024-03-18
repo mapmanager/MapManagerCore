991c95d149d36bbd64cc113d41574ea54b447f9f
Robert Cudmore <robert.cudmore@gmail.com>
2024-03-18 10:15:31 -0700

    before merge

---
+++ mapmanagercore/__init__.py
@@ -0,0 +1,5 @@
+from .annotations.file_loader import MapAnnotations
+from .image.base import ArrayImageLoaderBuilder
+
+# added when converting to pip install with setup.py
+from ._version import __version__
