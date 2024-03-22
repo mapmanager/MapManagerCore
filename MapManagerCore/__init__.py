47788a1df6c8e5eec1711634cc08babb308bf724
Robert Cudmore <robert.cudmore@gmail.com>
2024-03-22 16:30:54 -0700

    after removing capital MapManagerCore

--- MapManagerCore/__init__.py
+++
@@ -1,6 +0,0 @@
-from .annotations import Annotations as MapAnnotations
-from .loader.imageio import MultiImageLoader
-from .loader.mmap import MMapLoader
-
-# added when converting to pip install with setup.py
-from ._version import __version__
