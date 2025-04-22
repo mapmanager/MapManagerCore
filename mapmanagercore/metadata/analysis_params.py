93a95f4da2eeec12cf0e710de2d5590b4763aca4
Robert Cudmore <robert.cudmore@gmail.com>
2025-04-22 11:29:47 -0400

    push for j 202504

---
+++ mapmanagercore/metadata/analysis_params.py
@@ -0,0 +1,99 @@
+from pprint import pprint
+
+import dataclasses
+from dataclasses_json import dataclass_json
+
+from mapmanagercore.metadata._metadata3 import _metadataBase
+# from mapmanagercore.exceptions import MetadataError
+from mapmanagercore.logger import logger
+
+
+# trying to generalize creation of metadata dict for dataclass.field()
+@dataclasses.dataclass
+class _fieldMetadata():
+    description: str = ''
+    title: str = ''
+    units: str = 'um'
+
+def _getMetadata(
+        description: str = '',
+        title: str = '',
+        units: str = 'um',
+):
+    _dict = {
+        'description': description,
+        'title': title,
+        'units': units,
+    }
+    fieldMetadata = _fieldMetadata(**_dict)  # unpack dict
+    return dataclasses.asdict(fieldMetadata)
+
+@dataclass_json
+@dataclasses.dataclass
+class AnalysisParams(_metadataBase):
+    """Analysis parameters for a single timepoint
+    
+    Will also be used for all timepoints in a mmap.
+    """
+
+    # """Manually increment this when we add to this class."""
+    version: float = dataclasses.field(
+        default=0.6, 
+        metadata={
+            'description': 'Save metadata version.'
+            })
+
+    # spines
+    brightestPathDistance: int = dataclasses.field(
+        default=10, 
+        metadata={
+            'description': 'Points along the tracing to find spine connection (anchor).'
+            })
+
+    brightestPathChannel: int = dataclasses.field(
+        default=1, 
+        metadata={
+            'description': 'Image color channel to find brightest connection of spine.'
+            })
+    
+    brightestPathZSpread: int = dataclasses.field(default=3, metadata={'description': 
+                                                             'Number of image slices for max project to find brightest connection of spine.'
+                                                             })
+    roiExtend: int = dataclasses.field(default=4, metadata={'description': 
+                                                             'Number of pixels to extend spine head for spine ROI.'
+                                                             })
+    roiRadius: int = dataclasses.field(default=4, metadata={'description': 
+                                                             'Width of spine ROI.'
+                                                             })
+    
+    # segments
+    segmentRadius: int = dataclasses.field(default=4, metadata={'description': 
+                                                             'Radius of segment tracing.'
+                                                             })
+    segmentTracingMaxDistance: int = dataclasses.field(default=90, metadata={'description': 
+                                                             'Max distance to trace a brightest path.'
+                                                             })
+    backgroundRoiGridPoints: int = dataclasses.field(default=5, metadata={'description': 
+                                                             'Number of points in grid (nxn) to calculate background ROI.'
+                                                             })
+    backgroundRoiGridOverlap: float = dataclasses.field(default=0.1, metadata={'description': 
+                                                             'Overlap of background grid points.'
+                                                             })
+    brightestPathTracing: bool = \
+        dataclasses.field(default=False,
+                          metadata=_getMetadata(
+                              description='Turn brightest path tracing on and off',
+                              title='Brightest Path Tracing',
+                              units='',
+                              )
+        )
+
+if __name__ == '__main__':
+    _dict = _getMetadata(
+        description='xxx',
+        title='yyy',
+        units='um',
+    )
+    logger.info('main')
+    pprint(_dict)
+    pprint(_dict)
